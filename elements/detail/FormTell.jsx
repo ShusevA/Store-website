02acee0416fa0690b426827343547a561b13dcd4
ShusevA <60173266+ShusevA@users.noreply.github.com>
2020-12-03 16:18:07 +0200

    Update FormTell.jsx

--- elements/detail/FormTell.jsx
+++ elements/detail/FormTell.jsx
@@ -6,13 +6,15 @@ import React from "react";
 export default class FormTell extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {tell: "",  tellAll: ""}
+        this.state = { tell: "", tellAll: "" }
         this.inputRef = React.createRef();
         this.check = this.check.bind(this);
+        this.start = this.start.bind(this);
+        this.addTell = this.addTell.bind(this);
     }
 
-    componentDidUpdate(){
-        if(this.state.tellAll > 0 ) {
+    componentDidUpdate() {
+        if (this.state.tellAll > 0) {
             document.querySelector(".checkTell").disabled = false;
             document.querySelector(".checkTell").style.cursor = "pointer";
             document.querySelector(".checkTell").style.backgroundColor = "#ff8900"
@@ -20,37 +22,61 @@ export default class FormTell extends React.Component {
             document.querySelector(".checkTell").disabled = true;
             document.querySelector(".checkTell").style.cursor = "";
             document.querySelector(".checkTell").style.backgroundColor = "#717381"
-       }
+        }
     }
 
     check(e) {
-        if (!this.inputRef.current.value.match(/^\+/)) {
-            e.target.value = "+38" + this.inputRef.current.value;
-        }
-        if (this.state.tell.length == 13) {
-            if (this.inputRef.current.value.match(/^\+\d{12,}/)) {
+        if (e.target.value.length == 22) {
+            if (this.inputRef.current.value.match(/\+\d\d\(\d\d\d\)\s\d\d\d\s\-\s\d\d\s\-\s\d\d/)) {
                 document.getElementById('phone-mask').style.borderColor = '#9c9c9c';
-                this.setState({tellAll: 1});
+                this.setState({ tellAll: 1 });
             }
         } else {
             document.getElementById('phone-mask').style.borderColor = 'red';
-            this.setState({tellAll: 0});
+            this.setState({ tellAll: 0 });
         }
     }
 
+    start(e) {
+        if (e.target.value.match(/\+\d\d\(\d\d\d\)\s\d\d\d\s\-\s\d\d\s\-\s\d\d/)) {
+        this.setState({ tellAll: 1 });
+        }
+        if (e.target.value.length == 17) {
+            if (e.target.value.match(/\+\d\d\(\d\d\d\)\s\d\d\d\s\-\s\d\d/)) {
+                e.target.value += " - "
+            }
+        }
 
-render() {
+        if (e.target.value.length == 12) {
+            if (e.target.value.match(/\+\d\d\(\d\d\d\)\s\d\d\d/)) {
+                e.target.value += " - "
+            }
+        }
+        if (e.target.value.length == 7) {
+            if (e.target.value.match(/\+\d\d\(\d\d\d/)) {
+                e.target.value += ") ";
+            }
+        }
+    }
 
-    return (
-        <div className="formTell">
-            <p className="formTell-title">Перезвоните мне</p>
-            <form action="" method="get" className="formTell-form">
-                <label htmlFor="" className="formTell-ladel">Номет телефона</label>
-                <input type="text" name="tell" id="phone-mask" placeholder="+38(0__) ___ - __ - __" onChange={(e)=> this.setState({ tell: e.target.value })} ref={this.inputRef} maxLength={13} onBlur={this.check} />
-                <button type="submit" className="checkTell" disabled>Перезвоните мне</button>
-            </form>
+    addTell(e) {
+        e.target.value = "+38(0"
+    }
 
-        </div>
-    )
-}
+    render() {
+
+        return (
+            <div className="formTell">
+                <p className="formTell-title">Перезвоните мне</p>
+                <form action="" method="get" className="formTell-form">
+                    <label htmlFor="" className="formTell-ladel">Номет телефона</label>
+                    <input type="text" name="tell" id="phone-mask" placeholder="+38(0__) ___ - __ - __"
+                         onChange={(e)=> this.setState({ tell: e.target.value })}
+                        ref={this.inputRef} maxLength={22} onBlur={this.check} onFocus={this.addTell} onInput={this.start} />
+                    <button type="submit" className="checkTell" disabled>Перезвоните мне</button>
+                </form>
+
+            </div>
+        )
+    }
 }
